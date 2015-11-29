9e78e4bb7b623626e19ca4e5794b997ad879e4c3
박중언 <ideal8901@kaist.ac.kr>
2015-11-29 22:05:39 +0900

    commit : check existence of rating

--- diver/diver/views.py
+++ diver/diver/views.py
@@ -63,6 +63,13 @@ def like(request, id, score):
     if request.method == 'GET':
         item = Item.objects.filter(id = id)
         if item != []:
+
+            item_pref = ItemPref.objects.filter(item_id = id, user_id = request.user.id)
+
+            if item_pref != []:
+                # item 값이 벼하는 시점
+                pass
+
             # customer = Customer.objects.filter(user_id = request.user.id)
             # customer.like(Item.objects.filter(id = id), like)
             item_pref = ItemPref(item_id = id, user_id = request.user.id, score = score)
